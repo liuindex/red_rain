147e1b924a4b1c81582c287329a88bcdc5909942
God <zm1506716201@163.com>
2019-12-22 22:42:02 +0800

    初始化模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    红包雨
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
+<style lang="scss" scoped>
+  .home{
+    width: 100%;
+    height: 100%;
+    background: rgba($color: #000000, $alpha: 0.7)
+  }
+</style>
