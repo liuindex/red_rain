298aea6a99fa990198ab21ff0d6cbb1cc95f1fd0
God <zm1506716201@163.com>
2019-12-28 19:37:43 +0800

    config配置

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,25 @@
+/**
+ * Vue CLI参考：https://cli.vuejs.org/zh/config/
+ * webpack参考：https://webpack.js.org/configuration/
+ * webpack中文：https://www.webpackjs.com/concepts/
+ */
+
 const webpack = require('webpack')
- 
+
 module.exports = {
-    configureWebpack: {
+  devServer: {
+    port: 8889,
+    hotOnly: true, // 热更新
+    host: 'apprentice.com',
+    open: true,
+  },
+  configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
-            $:"jquery",
-            jQuery:"jquery",
-            "windows.jQuery":"jquery"
-        })
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+      })
     ]
   }
 }
